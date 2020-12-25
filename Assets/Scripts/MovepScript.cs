5f435594c709e29f0c8e49986f17a68512c4fac4
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-12-25 23:39:00 +0900

    MovepScript作成

---
+++ Assets/Scripts/MovepScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovepScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Holizontal")*Time.deltaTime, 0f, 0f);
+        Debug.Log(transform.position.x);
+    }
+}
