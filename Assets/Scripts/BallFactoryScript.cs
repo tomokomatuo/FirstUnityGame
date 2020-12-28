7e14252675955f61b4e71cab7e7a758e9597a259
tomoko matsuo <matsuo09212001@yahoo.co.jp>
2020-12-28 21:22:05 +0900

    BallScript生成

--- Assets/Scripts/BallFactoryScript.cs
+++ Assets/Scripts/BallFactoryScript.cs
@@ -8,6 +8,12 @@ public class BallFactoryScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Instantiate(ball, transform.position, transform.rotation);
+        InvokeRepeating("SpawnBall", 0f, 1f);
+    }
+    void SpawnBall()
+    {
+        Instantiate(ball, new Vector3(Random.Range(-5f, 5f), transform.position.y, transform.position.z), transform.rotation);
 
     }
 
